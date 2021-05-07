cafe5a8daa64662a285f519d7e7c666368bac5cf
LucasSardo <80767903+LucasSardo@users.noreply.github.com>
2021-05-07 11:07:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,19 +6,13 @@
 
 
             app.use('/css',express.static('css'));
-            app.use('/uploads',express.static('uploads'))
-            app.use('/views',express.static('views'))
-            app.use('/views/layouts',express.static('views/layouts'))
-            app.use('/',express.static('/'))
+
+
             const hdbars = require('express-handlebars');
             //app.engine('handlebars',hdbars  ({defaultLayout:'Main'}));
-            //app.engine('handlebars', hdbars({extname: "handlebars",defaultLayout: "Main", layoutsDir: "", }));
-            //app.engine('handlebars',hdbars  ({defaultLayout:'Main'}));
-            app.engine('handlebars', hdbars({
-            extname: "handlebars",
-            defaultLayout: "Main",
-            layoutsDir: path.join(__dirname, 'views/layouts' 
-            }));
+          //  app.engine('handlebars', hdbars({extname: "handlebars",defaultLayout: "Main", layoutsDir: "./views/layouts", }));
+            app.engine('handlebars', hdbars({extname: "handlebars", defaultLayout: "Main",layoutsDir: path.join(__dirname, 'views/layouts' }));
+
 
             app.set('view engine','handlebars');
 
@@ -27,7 +21,7 @@
             const rotaUsuarios = require('./routes/usuarios');
 
             app.use(morgan('dev'));//EXTENSAO QUE DA RESPOSTAS DE GET E POST
-
+            app.use('/uploads',express.static('uploads'))
             app.use(bodyParser.urlencoded({extended: false}));
             app.use(bodyParser.json());
 
@@ -37,12 +31,12 @@
                     'Access-Control-Allow-Headers', 
                     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
                 );
-                
+
                 if (req.method==='OPTIONS'){
                     res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET, OPTIONS');
                     return res.status(200).send({})
                 }
-            
+
                 next();
             })
 
@@ -53,14 +47,9 @@
             app.get("/", (req, res) =>{ 
                 //res.send('ROTA DO BasasARRA'); 
                 //res.sendFile(__dirname+'/views/index.html')
+                return res.render(__dirname+'/views/index');
                 //return res.send("você está no barra")
-                //return res.render(__dirname+'/views/index');
-                //return res.render('index'); //{"erro":{"mensagem":"ENOENT: no such file or directory, open '/app/views/layouts/Main.handlebars'"}}
-                //return res.render('Main'); //{"erro":{"mensagem":"Failed to lookup view \"Main\" in views directory \"/app/views\""}}
-                //return res.render(__dirname+'/views/layouts/Main'); //{"erro":{"mensagem":"Failed to lookup view \"/app/views/layouts/Main\" in views directory \"/app/views\""}}
-                //return res.render(__dirname+'/views/layouts/Main.handlebars');// "erro":{"mensagem":"Failed to lookup view \"/app/views/layouts/Main.handlebars\" in views directory \"/app/views\""}}
-                //return res.render('Main.handlebars'); //{"erro":{"mensagem":"Failed to lookup view \"Main.handlebars\" in views directory \"/app/views\""}}
-                return res.render('index');
+
             }) 
 
 
